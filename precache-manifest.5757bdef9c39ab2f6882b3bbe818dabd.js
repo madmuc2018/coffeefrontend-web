04ea4ce5599ce47e21cacaee647f27d90c2b00c6
Thai Le <thailekha@yahoo.com>
2019-03-26 00:24:16 +0000

    Updates

---
+++ precache-manifest.5757bdef9c39ab2f6882b3bbe818dabd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1710af2a3f2d0f2c39a2110e3901d733",
+    "url": "/coffeefrontend-web/static/media/logo.1710af2a.png"
+  },
+  {
+    "revision": "f1a065cecc232a89081f",
+    "url": "/coffeefrontend-web/static/js/runtime~main.c146627e.js"
+  },
+  {
+    "revision": "b1d59f844de1791245cc",
+    "url": "/coffeefrontend-web/static/js/main.f2142d54.chunk.js"
+  },
+  {
+    "revision": "74db4715df760b990962",
+    "url": "/coffeefrontend-web/static/js/2.dea13185.chunk.js"
+  },
+  {
+    "revision": "b1d59f844de1791245cc",
+    "url": "/coffeefrontend-web/static/css/main.033ddfa2.chunk.css"
+  },
+  {
+    "revision": "67a80d5940bb671c14ed0d37982cd46e",
+    "url": "/coffeefrontend-web/index.html"
+  }
+];
